f22e07efc6efec777e18c2b932b78a36beca630f
Sam-Guliker <samguliker@hotmail.com>
2023-02-03 14:04:58 +0100

    clean up cms

--- sanity-portfolio/schemas/post.ts
+++ sanity-portfolio/schemas/post.ts
@@ -20,24 +20,12 @@ export default defineType({
       },
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
       options: {
         hotspot: true,
       },
-    }),
-    defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
     }),
     defineField({
       name: 'publishedAt',
